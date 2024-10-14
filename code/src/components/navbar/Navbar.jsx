f24c94a9c7d26923e5e6206f09789e3202bcd703
nipun2403 <nipun24sharma@gmail.com>
2024-10-15 01:15:40 +0530

    Minor Layout Changes

--- code/src/components/navbar/Navbar.jsx
+++ code/src/components/navbar/Navbar.jsx
@@ -2,40 +2,13 @@ import React, { useState, useEffect } from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
-  const [isGray, setIsGray] = useState(false);
-
-  useEffect(() => {
-    const changeNavDiv = document.getElementById("changeNav");
-
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        setIsGray(entry.isIntersecting);
-      },
-      { threshold: 0.4 }
-    );
-
-    if (changeNavDiv) {
-      observer.observe(changeNavDiv);
-    }
-
-    return () => {
-      if (changeNavDiv) {
-        observer.unobserve(changeNavDiv);
-      }
-    };
-  }, []);
-
   return (
     <nav
-      className={`fixed top-0 left-0 w-full flex justify-between items-center px-20 pr-[7rem] pt-10 pb-4 text-lg ${
-        isGray
-          ? "bg-black bg-opacity-60 "
-          : "bg-transparent border-b-2 border-white/20"
-      } text-white  z-50 transition-all duration-300`}
+      className={`fixed top-0 left-0 w-full flex justify-between items-center px-20 pr-[7rem] pt-10 pb-4 text-lg bg-black bg-opacity-60 text-white  z-50 transition-all duration-300`}
     >
       {/* Left Section (Logo) */}
       <div className="flex items-center">
-        <span className="text-2xl font-bold">TATA LOGO</span>
+        <span className="text-2xl font-bold">TATA VALUE HOMES</span>
       </div>
 
       {/* Center Section (Navigation Links) */}
--- code/src/pages/home/Home2.jsx
+++ code/src/pages/home/Home2.jsx
@@ -1,4 +1,4 @@
-import HomeBanner from "../../assets/HomeBanner.jpg";
+import HomeBanner from "../../assets/HomeBanner2.jpg";
 import SeamlessCarousel from "../../components/carousel/SeamlessCarousel";
 import QuotationForm from "../../components/form/QuotationForm";
 import caller from "../../assets/call.png";
@@ -18,15 +18,17 @@ const Home = () => {
         />
 
         {/* EOI Div - Positioned on the bottom right of the banner */}
-        <div className="absolute bottom-32 left-10 bg-black bg-opacity-50 text-white text-6xl px-4 py-2">
-          <Typewriter
-            options={{
-              strings: ["Accepting EOI NOW!!!!"],
-              autoStart: true,
-              loop: true,
-              deleteSpeed: 50,
-            }}
-          />
+        <div className="absolute bottom-[23rem] left-[25rem] bg-black bg-opacity-50 text-white text-6xl px-4 py-2">
+          <a href="#contactUs">
+            <Typewriter
+              options={{
+                strings: ["Accepting EOI NOW!!!!"],
+                autoStart: true,
+                loop: true,
+                deleteSpeed: 50,
+              }}
+            />
+          </a>
         </div>
       </div>
 
@@ -43,8 +45,8 @@ const Home = () => {
           <div>
             {/* Box 1  */}
             <div className="flex gap-10 items-center ">
-              <p className="basis-1/2 text-4xl leading-relaxed">
-                New Haven By TATA
+              <p className="basis-2/3 text-4xl leading-relaxed">
+                New Haven Bahadurgarh, Haryana
               </p>
               <p>
                 Welcome to the home you've been waiting for, where life unfolds
@@ -56,25 +58,9 @@ const Home = () => {
 
             {/* BOX 2 */}
             <div className="mt-20 flex justify-between">
-              <div className="">
-                <p className={smallHeading}>Location</p>
-                <p className={bigHeading}>Bahadurgarh, Haryana</p>
-              </div>
               <div>
                 <p className={smallHeading}>Status</p>
                 <p className={bigHeading}>Bookings Open For Phase 2</p>
-              </div>
-              <div>
-                <p className={smallHeading}>Configurations</p>
-                <p className={bigHeading}>1 BHK, 2 BHK, 3 BHK</p>
-              </div>
-            </div>
-
-            {/* BOX 3 */}
-            <div className="mt-20 flex justify-between">
-              <div>
-                <p className={smallHeading}>Area</p>
-                <p className={bigHeading}>10 Acres Approx</p>
               </div>
               <div className="">
                 <p className={smallHeading}>Booking amount</p>
@@ -83,9 +69,10 @@ const Home = () => {
                   <p className={bigHeading}>3 BHK - 5 Lakhs</p>
                 </div>
               </div>
+
               <div>
-                <p className={smallHeading}>Designer</p>
-                <p className={bigHeading}>CP Kukreja Architects</p>
+                <p className={smallHeading}>Configurations</p>
+                <p className={bigHeading}>1 BHK, 2 BHK, 3 BHK</p>
               </div>
             </div>
           </div>
@@ -105,13 +92,14 @@ const Home = () => {
         <div className="fixed bottom-[2rem] right-[1rem] z-50 flex items-center">
           <a
             href="tel:+1234567890"
-            className="flex items-center"
+            className="flex-col justify-center items-center align-center"
           >
             <img
               src={caller}
               alt="Call Us"
               className="w-14 h-14 rounded-full shadow-md mr-2"
             />
+            <p>+91-9821987198</p>
           </a>
         </div>
       </div>
